9bf22c8b9541124e1e6fdec1949002cc35f43248
tin33392js <calsharp0202@yahoo.com>
2022-09-01 19:21:59 +0700

    improve layout for better display goals

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -7,8 +7,12 @@ function GoalItem({ goal, index }) {
 
 	return (
 		<div className="goal">
-			<div>{new Date(goal.createdAt).toLocaleString('en-UK')} </div>
-			<div style={{ fontStyle: 'italic', fontSize: '50%' }}>-{index}-</div>
+			<div className="number">
+				{new Date(goal.createdAt).toLocaleString('en-UK')}
+			</div>
+			<div className="number" style={{ fontStyle: 'italic', fontSize: '50%' }}>
+				-{index}-
+			</div>
 			<h2>{goal.text}</h2>
 			<button
 				onClick={(e) => {
